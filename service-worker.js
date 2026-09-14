3bd3c52273991d6a1295c4f8c3e0a2b35e7a85fb
noagrp <lihwlee11@gmail.com>
2026-09-14 11:21:09 +0800

    Cache LiteEditor PWA icons

--- service-worker.js
+++ service-worker.js
@@ -1,8 +1,12 @@
-const CACHE_NAME = 'liteditor-v1';
+const CACHE_NAME = 'liteditor-v2';
 const APP_SHELL = [
   './',
   './index.html',
-  './manifest.webmanifest?v=1'
+  './manifest.webmanifest?v=2',
+  './favicon-32x32.png?v=2',
+  './icon-192.png?v=2',
+  './icon-512.png?v=2',
+  './apple-touch-icon.png?v=2'
 ];
 
 self.addEventListener('install', event => {
